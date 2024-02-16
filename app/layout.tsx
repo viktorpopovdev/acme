74deaea856f7e2e12bf39437d97eb3b68779261d
Viktor <viktor@Viktor-MacBook-Pro.local>
2024-02-16 13:41:36 +0200

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import './ui/global.css';
+import { inter } from './ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/* <body>{children}</body> */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
